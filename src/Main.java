f0d61bdec6492cc1d7b0605cb5f21f32bb05cdb6
Makayla <makaylawms0426@gmail.com>
2023-02-07 21:15:38 -0800

    Have added the addRoot method and the print method

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
     public static void main(String[] args)
     {
         Tree myTree = new Tree();
+
+        myTree.addRoot(45);
+
+        myTree.printInOrder(myTree.getRoot());
     }
+
+
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,8 +1,11 @@
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Tree
 {
     //variables
     private TreeNode root;
+    private Queue<TreeNode> queue = new LinkedList<>();
 
     //constructor
     public Tree()
@@ -10,6 +13,34 @@ public class Tree
         root = null;
     }
 
+    public TreeNode getRoot()
+    {
+        return root;
+    }
+
+    //create root
+    public void addRoot(int x)
+    {
+        root = new TreeNode(x);
+        queue.add(root);
+    }
+
+
+    //print out tree (goes left, root, then right)
+    public void printInOrder(TreeNode node)
+    {
+        //if the node is null return nothing
+        if(node == null)
+        {
+            return;
+        }//end if
+        //recursion to go the furthest left as possible
+        printInOrder(node.left);
+        //print out the value
+        System.out.print(node.value + " ");
+        //recursion to go the furthest right on the same level and do the same thing
+        printInOrder(node.right);
+    }//end printInOrder
 
 
     private class TreeNode
